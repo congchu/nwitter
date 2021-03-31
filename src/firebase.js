75dda026d79bfbc28c74ad4717cea01852ac6526
congchu <cookie00421@gmail.com>
2021-03-31 16:34:19 +0900

    파이어베이스 셋팅까지 완료

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDa_fPZdLCP1c9QqLeoHWz45aKCRvzpP0Y",
+  authDomain: "nwitter-f8061.firebaseapp.com",
+  projectId: "nwitter-f8061",
+  storageBucket: "nwitter-f8061.appspot.com",
+  messagingSenderId: "1089598713551",
+  appId: "1:1089598713551:web:73216a6182c9f761b591ba",
+};
+
+export default firebase.initializeApp(firebaseConfig);
