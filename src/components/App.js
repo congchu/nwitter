87541373c872384bf0185f0a3441916a9a7b331b
congchu <cookie00421@gmail.com>
2021-03-31 16:48:52 +0900

    페이지 4개 추가, 라우터 추가하고 동작 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import AppRouter from "./Router";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
