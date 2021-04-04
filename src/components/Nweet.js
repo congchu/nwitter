43aa3ff6e863bebf7111602dfcafaf2230a57cda
congchu <cookie00421@gmail.com>
2021-04-04 16:32:05 +0900

    느윗 삭제 로직 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,10 +1,20 @@
+import { dbService } from "fbase";
 import React from "react";
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && <button>Delete Nweet</button>}
-    {isOwner && <button>Edit Nweet</button>}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = () => {
+    const ok = window.confirm("Are you sure you want to delete thie nweet?");
+    if (ok) {
+      dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && <button onClick={onDeleteClick}>Delete Nweet</button>}
+      {isOwner && <button>Edit Nweet</button>}
+    </div>
+  );
+};
 
 export default Nweet;
