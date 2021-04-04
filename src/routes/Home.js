55877a51f514c068b5b8d33a5d1b4188febd5e54
congchu <cookie00421@gmail.com>
2021-04-04 14:25:27 +0900

    느윗 리스트 불러오는 기능 적용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,29 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 export default () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObejct = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObejct, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({ nweet, createAt: Date.now() });
     setNweet("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
@@ -26,6 +42,13 @@ export default () => {
           maxLength={120}
           value={nweet}
         />
+        <div>
+          {nweets.map((nweet) => (
+            <div key={nweet.id}>
+              <h4>{nweet.nweet}</h4>
+            </div>
+          ))}
+        </div>
         <input type="submit" value="Nweet" />
       </form>
     </div>
