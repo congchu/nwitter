ad7f402905c58d402421f6a2800cd0d269a39615
congchu <cookie00421@gmail.com>
2021-04-05 15:30:28 +0900

    유저 정보 수정 하면 rerender되도록 수정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { authService, dbService } from "fbase";
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
   useEffect(() => {
@@ -32,6 +32,7 @@ const Profile = ({ userObj }) => {
       await userObj.updateProfile({
         displayName: newDisplayName,
       });
+      refreshUser();
     }
   };
 
